8fb0c51bdbdda013f977acaab47077ab46339f00
KhoaChaos6969 <khoaplagcs210326@fpt.edu.vn>
2023-08-15 21:19:15 +0700

    Add project files.

---
+++ Lab8UsersAndRoles/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Lab8UsersAndRoles.Models;
+
+namespace Lab8UsersAndRoles.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Lab8UsersAndRoles.Models.ApplicationRole> ApplicationRole { get; set; } = default!;
+    }
+}
---
+++ Lab8UsersAndRoles/Models/ApplicationRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Lab8UsersAndRoles.Models
+{
+    public class ApplicationRole:IdentityRole
+    {
+        public string? Descriptions;
+    }
+}
